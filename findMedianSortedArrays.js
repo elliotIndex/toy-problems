c37d1783dd3381dce440c622c937d27311b7b981
Elliot Plant <elliotaplant@gmail.com>
2016-05-13 09:41:55 -0700

    Continue working on findMedianSortedArrays

--- findMedianSortedArrays.js
+++ findMedianSortedArrays.js
@@ -4,5 +4,11 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-  
+  //
 };
+
+var arr1 = [4,6,7,8,9,10,11];
+var arr2 = [1,2,3,4,5];
+
+[n,n,3,n,n,...,n,n,8,n,n,n]
+console.log(findMedianSortedArrays(arr1, arr2));
