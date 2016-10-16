4df99ceeef2bb6047a47f131cec849c783488693
Tanner Velten <tannervelten@gmail.com>
2016-10-15 17:01:48 -0700

    Solve isAnagram

---
+++ isAnagram.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+var isAnagram = function(s, t) {
+
+    var sArr = s.split('').sort();
+    var tArr = t.split('').sort();
+
+    if (sArr.length !== tArr.length) {
+        return false;
+    }
+
+    for (var i = 0; i < sArr.length; i++) {
+        if (sArr[i] !== tArr[i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+console.log(isAnagram(anagram, nagaram));
